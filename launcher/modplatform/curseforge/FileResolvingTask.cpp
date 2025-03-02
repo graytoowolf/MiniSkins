62103af8c89bfe642d361c0da900808b97aefdca
graywolf <graytoowolf@outlook.com>
2025-03-02 16:18:07 +0800

    优化mod比较

--- launcher/modplatform/curseforge/FileResolvingTask.cpp
+++ launcher/modplatform/curseforge/FileResolvingTask.cpp
@@ -3,12 +3,13 @@
 #include "FileSystem.h"
 #include "Json.h"
 
-namespace {
-    const char * metabase = "https://api.curseforge.com/v1/mods";
+namespace
+{
+    const char *metabase = "https://api.curseforge.com/v1/mods";
 }
 
-CurseForge::FileResolvingTask::FileResolvingTask(shared_qobject_ptr<QNetworkAccessManager> network, CurseForge::Manifest& toProcess,const QString& path)
-    : m_network(network), m_toProcess(toProcess),m_path(path)
+CurseForge::FileResolvingTask::FileResolvingTask(shared_qobject_ptr<QNetworkAccessManager> network, CurseForge::Manifest &toProcess, const QString &path)
+    : m_network(network), m_toProcess(toProcess), m_path(path)
 {
 }
 
@@ -22,7 +23,8 @@ void CurseForge::FileResolvingTask::executeTask()
     setStatus(tr("Parsing directory"));
     QJsonObject requestObject;
     QJsonArray modIdsArray;
-    for (auto &file : m_toProcess.files) {
+    for (auto &file : m_toProcess.files)
+    {
         modIdsArray.append(file.projectId);
     }
     requestObject["modIds"] = modIdsArray;
@@ -31,14 +33,13 @@ void CurseForge::FileResolvingTask::executeTask()
     netRequest.setRawHeader("x-api-key", APPLICATION->curseAPIKey().toUtf8());
     netRequest.setHeader(QNetworkRequest::ContentTypeHeader, "application/json");
 
-    m_rep = m_network->post(netRequest,QJsonDocument(requestObject).toJson());
+    m_rep = m_network->post(netRequest, QJsonDocument(requestObject).toJson());
     connect(m_rep, &QNetworkReply::finished, this, &CurseForge::FileResolvingTask::downloadFinished);
-
-
 }
 void CurseForge::FileResolvingTask::downloadFinished()
 {
-    if (m_rep->error() != QNetworkReply::NoError) {
+    if (m_rep->error() != QNetworkReply::NoError)
+    {
         qDebug() << "Network Error:" << m_rep->errorString();
         m_rep->deleteLater();
         return;
@@ -58,20 +59,25 @@ void CurseForge::FileResolvingTask::downloadFinished()
 
 void CurseForge::FileResolvingTask::processModData(const QJsonArray &dataArray)
 {
-    for (const auto &dataValue : dataArray) {
+    for (const auto &dataValue : dataArray)
+    {
         auto modObj = dataValue.toObject();
         int classId = modObj.value("classId").toInt();
         int m_id = modObj.value("id").toInt();
         QString m_name = modObj.value("name").toString();
-        for (auto &m_file : m_toProcess.files) {
-            if (m_file.projectId == m_id){
+        for (auto &m_file : m_toProcess.files)
+        {
+            if (m_file.projectId == m_id)
+            {
                 m_file.targetFolder = getTargetFolderByClassId(classId);
             }
         }
-        if (APPLICATION->isModBlacklisted(m_id)){
+        if (APPLICATION->isModBlacklisted(m_id))
+        {
             QString modName = APPLICATION->getModNameFromBlacklist(m_id);
-            if (modName == "Provisional Name"){
-                APPLICATION->updateModBlacklistName(m_id,m_name);
+            if (modName == "Provisional Name")
+            {
+                APPLICATION->updateModBlacklistName(m_id, m_name);
             }
         }
     }
@@ -79,7 +85,8 @@ void CurseForge::FileResolvingTask::processModData(const QJsonArray &dataArray)
 
 QString CurseForge::FileResolvingTask::getTargetFolderByClassId(int classId)
 {
-    switch (classId) {
+    switch (classId)
+    {
     case 6:
         return "mods";
     case 6552:
@@ -98,93 +105,40 @@ void CurseForge::FileResolvingTask::prepareDownloads()
     QString InstanceDir = APPLICATION->settings()->get("InstanceDir").toString();
     QString m_instDir = QDir(InstanceDir).canonicalPath();
     QString m_modpacksid = APPLICATION->getID();
-    QString m_modpacksfile = FS::PathCombine(m_instDir,m_modpacksid);
-    QJsonArray modArray;
-    m_filePath = m_path;
-    QFile modFile(FS::PathCombine(m_modpacksfile,"mod.json"));
-    if (APPLICATION->isUpdating()){
-        if (modFile.exists()) {
-            if (modFile.open(QIODevice::ReadOnly)) {
-                QJsonDocument modDoc = QJsonDocument::fromJson(modFile.readAll());
-                modArray = modDoc.array();
-                m_filePath = FS::PathCombine(m_instDir,m_modpacksid);
-                modFile.close();
-            }
-        }
-    }
-    int index = 0;
-    //int indextask = 0;
-    QJsonArray modIdsArray;
-    for (auto &file : m_toProcess.files) {
-        auto projectIdStr = QString::number(file.projectId);
-        auto fileIdStr = QString::number(file.fileId);
-        bool shouldQueueForDownload = true;
-        if (!modArray.isEmpty()) {
-            for (int i = 0; i < modArray.size(); i++) {
-                QJsonObject modObject = modArray[i].toObject();
-                if (modObject["projectID"].toInt() == file.projectId) {
-                    if (modObject["fileID"].toInt() == file.fileId) {
-                        file.fileName = modObject["name"].toString();
-                        shouldQueueForDownload = false;
-                        break;
-                    } else {
-                        // 删除文件
-                        QString m_name = FS::PathCombine(m_modpacksfile, "minecraft",
-                                                       file.targetFolder,
-                                                       modObject["name"].toString());
-                        QFile::remove(m_name);
-                        // 从数组中移除该元素
-                        modArray.removeAt(i);
-                        i--; // 因为删除了元素，需要减少索引
-                        break;
-                    }
-                }
-            }
+    QString m_modpacksfile = FS::PathCombine(m_instDir, m_modpacksid);
 
-            // 检查并删除不再使用的mod
-            for (int i = modArray.size() - 1; i >= 0; i--) {
-                QJsonObject jsonFile = modArray[i].toObject();
-                const int projectID = jsonFile["projectID"].toInt();
-                auto fileIt = std::find_if(m_toProcess.files.begin(),
-                                         m_toProcess.files.end(),
-                                         [projectID](const File& file) {
-                                             return file.projectId == projectID;
-                                         });
-                if (fileIt == m_toProcess.files.end()) {
-                    // 删除文件
-                    QFile::remove(FS::PathCombine(m_modpacksfile, "minecraft",
-                                                file.targetFolder,
-                                                jsonFile["name"].toString()));
-                    // 从数组中移除
-                    modArray.removeAt(i);
-                }
-            }
+    QString m_mod = FS::PathCombine(m_modpacksfile, "mod.json");
 
-        }
-        if (shouldQueueForDownload) {
-            modIdsArray.append(fileIdStr);
-        }
-        index++;
-    }
-    if (modFile.open(QIODevice::WriteOnly)) {
-        QJsonDocument doc(modArray);
-        modFile.write(doc.toJson());
-        modFile.close();
+    CurseForge::ComparisonResult result = compareManifests(m_mod);
+
+    QString basePath = "minecraft";
+    QString minecraftPath = FS::PathCombine(m_modpacksfile, basePath);
+    if (!QDir(minecraftPath).exists())
+    {
+        basePath = ".minecraft";
     }
 
-    if (!modIdsArray.isEmpty())
+    for (const QString &fileName : result.filesToDelete)
+    {
+        QString m_name = FS::PathCombine(m_modpacksfile, basePath, "mods", fileName);
+        QFile::remove(m_name);
+    }
+
+    if (!result.filesToDownload.isEmpty())
     {
         QJsonObject requestObject;
-        requestObject["fileIds"] = modIdsArray;
+        requestObject["fileIds"] = result.filesToDownload;
         QString metaurl = QString("%1/files").arg(metabase);
         QNetworkRequest netRequest{QUrl(metaurl)};
         netRequest.setRawHeader("x-api-key", APPLICATION->curseAPIKey().toUtf8());
         netRequest.setHeader(QNetworkRequest::ContentTypeHeader, "application/json");
 
-        m_rep = m_network->post(netRequest,QJsonDocument(requestObject).toJson());
+        m_rep = m_network->post(netRequest, QJsonDocument(requestObject).toJson());
 
         connect(m_rep, &QNetworkReply::finished, this, &CurseForge::FileResolvingTask::netJobFinished);
-    } else {
+    }
+    else
+    {
         emitSucceeded();
     }
 }
@@ -196,23 +150,24 @@ void CurseForge::FileResolvingTask::netJobprogress(qint64 current, qint64 total)
 void CurseForge::FileResolvingTask::netJobFinished()
 {
     bool failed = false;
-    //int index = -1;
+    // int index = -1;
     QByteArray response = m_rep->readAll();
     m_rep->deleteLater();
     auto rootObj = QJsonDocument::fromJson(response).object();
     auto dataArray = rootObj.value("data").toArray();
-    for(const auto &dataValue : dataArray)
+    for (const auto &dataValue : dataArray)
     {
         auto modObj = dataValue.toObject();
-        int m_id = Json::requireInteger(modObj,"modId");
-        //int m_id = modObj.value("modId").toInt();
-        for (auto &m_file : m_toProcess.files) {
+        int m_id = Json::requireInteger(modObj, "modId");
+        // int m_id = modObj.value("modId").toInt();
+        for (auto &m_file : m_toProcess.files)
+        {
             if (m_file.projectId == m_id)
             {
                 m_file.fileName = Json::requireString(modObj, "fileName");
                 QString rawUrl = Json::requireString(modObj, "downloadUrl");
                 m_file.url = QUrl(rawUrl, QUrl::TolerantMode);
-                if(!m_file.url.isValid())
+                if (!m_file.url.isValid())
                 {
                     throw JSONValidationError(QString("Invalid URL: %1").arg(rawUrl));
                 }
@@ -220,31 +175,37 @@ void CurseForge::FileResolvingTask::netJobFinished()
         }
     }
 
-    m_filePath = FS::PathCombine(m_filePath,"mod.json");
-    QFile m_modFile(m_filePath);
-    if (m_modFile.open(QIODevice::WriteOnly)) {
+    QString m_modPath = FS::PathCombine(m_path, "mod.json");
+    QFile m_modFile(m_modPath);
+    if (m_modFile.open(QIODevice::WriteOnly))
+    {
         QJsonArray newArray;
-        for (const auto &file : m_toProcess.files) {
+        for (const auto &file : m_toProcess.files)
+        {
             QString m_name = file.fileName;
-            if (APPLICATION->isModBlacklisted(file.projectId)) {
+            bool required = file.required;
+            if (APPLICATION->isModBlacklisted(file.projectId))
+            {
                 m_name += ".disabled";
+                required = false;
             }
             QJsonObject fileObj{
                 {"projectID", file.projectId},
                 {"fileID", file.fileId},
-                {"required", true},
-                {"name", m_name}
-            };
+                {"required", required},
+                {"name", m_name}};
             newArray.append(fileObj);
         }
         QJsonDocument newDoc(newArray);
         m_modFile.write(newDoc.toJson());
         m_modFile.close();
-    } else {
+    }
+    else
+    {
         qDebug() << "Unable to open file for writing:" << m_filePath;
     }
 
-    if(!failed)
+    if (!failed)
     {
         emitSucceeded();
     }
@@ -252,4 +213,112 @@ void CurseForge::FileResolvingTask::netJobFinished()
     {
         emitFailed(tr("Some mod ID resolving tasks failed."));
     }
+}
+
+CurseForge::ComparisonResult CurseForge::FileResolvingTask::compareManifests(const QString &jsonFilePathA)
+{
+    CurseForge::ComparisonResult result;
+
+    // A文件不存在的情况
+    QFile fileA(jsonFilePathA);
+    if (!fileA.open(QIODevice::ReadOnly))
+    {
+        qWarning() << "无法打开A文件:" << jsonFilePathA;
+        for (const auto &file : m_toProcess.files) // 现在可以直接访问 m_toProcess
+        {
+            if (file.fileId != 0)
+            {
+                result.filesToDownload.append(file.fileId);
+            }
+        }
+        return result;
+    }
+
+    // 读取并解析A文件
+    QJsonDocument documentA = QJsonDocument::fromJson(fileA.readAll());
+    fileA.close();
+
+    if (!documentA.isArray())
+    {
+        qWarning() << "A文件JSON不是数组格式:" << jsonFilePathA;
+        return result;
+    }
+
+    QJsonArray aFiles = documentA.array();
+
+    // 创建A文件中projectID到完整信息的映射
+    struct AFileInfo
+    {
+        int fileID;
+        QString name;
+        bool required;
+    };
+    QMap<int, AFileInfo> aFileMap; // projectID -> FileInfo
+    for (const QJsonValue &value : aFiles)
+    {
+        QJsonObject obj = value.toObject();
+        int projectID = obj["projectID"].toInt();
+        AFileInfo info;
+        info.fileID = obj["fileID"].toInt();
+        info.name = obj["name"].toString();
+        info.required = obj["required"].toBool(true); // 默认为true
+
+        aFileMap[projectID] = info;
+    }
+
+    // 创建B文件中projectID的集合
+    QSet<int> bProjectIDs;
+
+    // 遍历B的manifest
+    for (auto &file : m_toProcess.files) // 注意:这里需要是引用才能修改
+    {
+        int projectID = file.projectId;
+        int fileID = file.fileId;
+
+        if (projectID == 0 || fileID == 0)
+        {
+            qWarning() << "B文件中存在无效的项目:" << projectID;
+            continue;
+        }
+
+        bProjectIDs.insert(projectID);
+
+        if (!aFileMap.contains(projectID))
+        {
+            result.filesToDownload.append(fileID);
+            continue;
+        }
+
+        const AFileInfo &aInfo = aFileMap[projectID];
+        if (aInfo.fileID != fileID)
+        {
+            result.filesToDownload.append(fileID);
+        }
+        else
+        {
+            // fileID相同时,同步name和required状态
+            file.fileName = aInfo.name;
+            file.required = aInfo.required;
+        }
+
+        // 如果A文件中required为false,则在B中也设为false
+        if (!aInfo.required)
+        {
+            file.required = false;
+        }
+    }
+
+    // 检查需要删除的文件
+    for (auto it = aFileMap.begin(); it != aFileMap.end(); ++it)
+    {
+        int projectID = it.key();
+        QString fileName = it.value().name;
+
+        if (!bProjectIDs.contains(projectID))
+        {
+            result.filesToDelete.append(fileName);
+        }
+    }
+
+    return result;
 }
--- launcher/modplatform/curseforge/FileResolvingTask.h
+++ launcher/modplatform/curseforge/FileResolvingTask.h
@@ -6,36 +6,45 @@
 
 namespace CurseForge
 {
-class FileResolvingTask : public Task
-{
-    Q_OBJECT
-public:
-    explicit FileResolvingTask(shared_qobject_ptr<QNetworkAccessManager> network, CurseForge::Manifest &toProcess,const QString& path = QString());
-    virtual ~FileResolvingTask() {};
-
-    const CurseForge::Manifest &getResults() const
+    struct ComparisonResult
     {
-        return m_toProcess;
-    }
+        QJsonArray filesToDownload; // 需要下载的fileID列表
+        QStringList filesToDelete;  // 需要删除的文件名列表
+    };
 
-protected:
-    virtual void executeTask() override;
+    class FileResolvingTask : public Task
+    {
+        Q_OBJECT
+    public:
+        explicit FileResolvingTask(shared_qobject_ptr<QNetworkAccessManager> network, CurseForge::Manifest &toProcess, const QString &path = QString());
+        virtual ~FileResolvingTask() {};
 
-protected slots:
-    void netJobFinished();
-    void netJobprogress(qint64 current, qint64 total);
-    void processModData(const QJsonArray &dataArray);
-    QString getTargetFolderByClassId(int classId);
-    void prepareDownloads();
-    void downloadFinished();
+        const CurseForge::Manifest &getResults() const
+        {
+            return m_toProcess;
+        }
 
-private: /* data */
-    shared_qobject_ptr<QNetworkAccessManager> m_network;
-    CurseForge::Manifest m_toProcess;
-    QVector<QByteArray> results;
-    NetJob::Ptr m_dljob;
-    QString m_path;
-    QString m_filePath;
-    QNetworkReply *m_rep;
-};
+    protected:
+        virtual void executeTask() override;
+
+    private:
+        CurseForge::ComparisonResult compareManifests(const QString &jsonFilePathA);
+
+    protected slots:
+        void netJobFinished();
+        void netJobprogress(qint64 current, qint64 total);
+        void processModData(const QJsonArray &dataArray);
+        QString getTargetFolderByClassId(int classId);
+        void prepareDownloads();
+        void downloadFinished();
+
+    private: /* data */
+        shared_qobject_ptr<QNetworkAccessManager> m_network;
+        CurseForge::Manifest m_toProcess;
+        QVector<QByteArray> results;
+        NetJob::Ptr m_dljob;
+        QString m_path;
+        QString m_filePath;
+        QNetworkReply *m_rep;
+    };
 }
