37ad49b06cb17717b4d1826cae8c397dd76e2020
Petr Mrázek <peterix@users.noreply.github.com>
2025-06-26 09:20:19 +0200

    Merge pull request #5626 from JoelTroch/feature/update_verify_java_versions_messages
    NOISSUE Update incorrect Java version messages

--- launcher/minecraft/launch/VerifyJavaInstall.cpp
+++ launcher/minecraft/launch/VerifyJavaInstall.cpp
@@ -55,27 +55,27 @@ void VerifyJavaInstall::executeTask() {
     // Java 17 requirement
     if (minecraftComponent->getReleaseDateTime() >= g_VersionFilterData.java17BeginsDate) {
         if (javaVersion.major() < 17) {
-            emit logLine("Minecraft 1.18 Pre Release 2 and above require the use of Java 17",
+            emit logLine("Minecraft 1.18 Pre Release 2 up to 24w13a require the use of Java 17",
                          MessageLevel::Fatal);
-            emitFailed(tr("Minecraft 1.18 Pre Release 2 and above require the use of Java 17"));
+            emitFailed(tr("Minecraft 1.18 Pre Release 2 up to 24w13a require the use of Java 17"));
             return;
         }
     }
     // Java 16 requirement
     else if (minecraftComponent->getReleaseDateTime() >= g_VersionFilterData.java16BeginsDate) {
         if (javaVersion.major() < 16) {
-            emit logLine("Minecraft 21w19a and above require the use of Java 16",
+            emit logLine("Minecraft 21w19a up to 1.18 Pre Release 1 require the use of Java 16",
                          MessageLevel::Fatal);
-            emitFailed(tr("Minecraft 21w19a and above require the use of Java 16"));
+            emitFailed(tr("Minecraft 21w19a up to 1.18 Pre Release 1 require the use of Java 16"));
             return;
         }
     }
     // Java 8 requirement
     else if (minecraftComponent->getReleaseDateTime() >= g_VersionFilterData.java8BeginsDate) {
         if (javaVersion.major() < 8) {
-            emit logLine("Minecraft 17w13a and above require the use of Java 8",
+            emit logLine("Minecraft 17w13a up to 21w18a require the use of Java 8",
                          MessageLevel::Fatal);
-            emitFailed(tr("Minecraft 17w13a and above require the use of Java 8"));
+            emitFailed(tr("Minecraft 17w13a up to 21w18a require the use of Java 8"));
             return;
         }
     }
